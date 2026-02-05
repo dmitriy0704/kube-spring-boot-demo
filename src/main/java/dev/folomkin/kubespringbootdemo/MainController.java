0597a740f47d97c5e8fde80b37dae82da96dfa51
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2026-02-05 13:29:46 +0300

    add config files

--- src/main/java/dev/folomkin/kubespringbootdemo/MainController.java
+++ src/main/java/dev/folomkin/kubespringbootdemo/MainController.java
@@ -1,5 +1,6 @@
 package dev.folomkin.kubespringbootdemo;
 
+import org.slf4j.helpers.Slf4jEnvUtil;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +12,11 @@ public class MainController {
     @GetMapping
     public String hello(){
         return "Hello from Kube";
+    }
+
+    @GetMapping("/health")
+    public String health(){
+        System.out.println("Status: OK");
+        return "Status: OK";
     }
 }
