0c5154c72bc9c5f7d50c94d3b2c18efd979bd312
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2026-02-05 10:45:59 +0300

    init app

---
+++ src/main/java/dev/folomkin/kubespringbootdemo/KubeSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.folomkin.kubespringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubeSpringBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KubeSpringBootDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/folomkin/kubespringbootdemo/KubeSpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.folomkin.kubespringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubeSpringBootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
